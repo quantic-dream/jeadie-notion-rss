03e71a4e6e96301179f2258f94199783fcf076c8
quantic-dream <147657456+quantic-dream@users.noreply.github.com>
2025-07-20 18:55:57 +0200

    Update main.go

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package main
 
 import (
 	"fmt"
+	"log"
 )
 
 func main() {
-
 	nDao, err := ConstructNotionDaoFromEnv()
 	if err != nil {
 		panic(fmt.Errorf("configuration error: %w", err))
 	}
 
+	// 🔄 Limpiar la base de datos antes de cargar nuevos feeds
+	err = nDao.CleanUnstarredContentDatabase()
+	if err != nil {
+		log.Fatalf("Error cleaning content database: %v", err)
+	}
+
+	// 🧾 Ejecutar tareas RSS
 	tasks := GetAllTasks()
 	errs := make([]error, len(tasks))
 	for i, t := range tasks {
